515e01e857627403eea7ba5dd6bee8b6792cf0fc
Lachlan Grogan <lachlan@sil3.com.au>
2016-09-29 17:15:18 +1000

    Doxygen Project Update

--- FIRMWARE/PROJECT_CODE/LCCM653__RLOOP__POWER_CORE/power_core.c
+++ FIRMWARE/PROJECT_CODE/LCCM653__RLOOP__POWER_CORE/power_core.c
@@ -54,6 +54,15 @@ void vPWRNODE__Process(void)
 	Luint8 u8Test;
 
 	//handle the init states here
+	/*
+	 * digraph G {
+	 * INIT_STATE__START -> INIT_STATE__COMMS [label = "After Setup"];
+	 * INIT_STATE__COMMS -> INIT_STATE__1_WIRE_START [label = "After configure serial comms devices"];
+	 * INIT_STATE__1_WIRE_START -> INIT_STATE__1_WIRE_SEARCH;
+	 * INIT_STATE__1_WIRE_SEARCH -> INIT_STATE__1_WIRE_SEARCH [label = "u8DS18B20_ADDX__SearchSM_IsBusy() == 1"];
+	 * INIT_STATE__1_WIRE_SEARCH -> INIT_STATE__1_WIRE_DONE  [label = "u8DS18B20_ADDX__SearchSM_IsBusy() == 0"];
+	 * }
+	 */
 	switch(sPWRNODE.sInit.sState)
 	{
 
