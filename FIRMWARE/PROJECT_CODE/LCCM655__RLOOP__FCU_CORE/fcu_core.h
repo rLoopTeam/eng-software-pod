af7dd5fa8cfb997d3b986a9da257b023f9d82d3d
Mila Antonova <mila.antonova@gmail.com>
2016-10-28 08:04:04 -0700

    Revert "Fcu brakes"

--- FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/fcu_core.h
+++ FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/fcu_core.h
@@ -11,12 +11,6 @@
 	#include <localdef.h>
 	#if C_LOCALDEF__LCCM655__ENABLE_THIS_MODULE == 1U
 
-
-		//defines
-		#include <LCCM655__RLOOP__FCU_CORE/fcu_core__defines.h>
-		#include <LCCM655__RLOOP__FCU_CORE/fcu_core__enums.h>
-
-
 		/*******************************************************************************
 		Defines
 		*******************************************************************************/
@@ -26,48 +20,6 @@
 		Structures
 		*******************************************************************************/
 
-		/** Main flight control structure */
-		struct _strFCU
-		{
-
-			/** Structure guard 1*/
-			Luint32 u32Guard1;
-
-
-			/** Brake Substructure */
-			struct
-			{
-
-				/** Linear position sensor detail */
-				struct
-				{
-					/** This is the sample from the ADC converter */
-					Luint16 u16ADC_Sample;
-
-					/** The zero value when the brakes are fully retracted */
-					Luint16 u16ADC_Zero;
-
-					/** Current ADC span to convert ADC units into engineering units */
-					Lfloat32 f32SystemSpan;
-
-					/** ADC value - Zero Value */
-					Lint32 s32ADC_Minus_Zero;
-
-					/** Percent of braking from 0.0 to 100.0*/
-					Lfloat32 f32BrakePosition_Percent;
-
-				}sMLP;
-
-
-
-			}sBrakes[C_FCU__NUM_BRAKES];
-
-
-			/** Structure guard 2*/
-			Luint32 u32Guard2;
-
-		};
-
 
 		/*******************************************************************************
 		Function Prototypes
@@ -76,11 +28,6 @@
 		DLL_DECLARATION void vFCU__Process(void);
 		
 		
-		//brakes
-		void vFCU_BRAKES__Init(void);
-		void vFCU_BRAKES__Process(void);
-
-
 	#endif //#if C_LOCALDEF__LCCM655__ENABLE_THIS_MODULE == 1U
 	//safetys
 	#ifndef C_LOCALDEF__LCCM655__ENABLE_THIS_MODULE
