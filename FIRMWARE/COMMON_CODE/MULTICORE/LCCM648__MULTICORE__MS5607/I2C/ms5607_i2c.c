995109195b7f1c9b8273b264c1d2c7f132b4a91b
40chans <edward6chan@psu.edu>
2016-10-31 20:49:30 -0700

    Add ms5607_i2c.c

---
+++ FIRMWARE/COMMON_CODE/MULTICORE/LCCM648__MULTICORE__MS5607/I2C/ms5607_i2c.c
@@ -0,0 +1,57 @@
+//I2C Interface layer
+#include "../ms5607.h"
+
+
+Lint16 s16MS5607_I2C__TxCommand(Luint8 u8DeviceAddx, E_TSYS01_REGS_T eRegister)
+{
+	Lint16 s16Return;
+
+#ifndef WIN32
+	//tx only the command
+	s16Return = s16RM4_I2C_USER__TxReg(u8DeviceAddx, (Luint8)eRegister);
+#else
+	//fake on win32
+	s16Return = 0;
+#endif
+	return s16Return;
+}
+
+//perform the actual transmission
+Lint16 s16MS5607_I2C__TxU8(Luint8 u8DeviceAddx, E_TSYS01_REGS_T eRegister, Luint8 u8Byte)
+{
+	Lint16 s16Return;
+
+#ifndef WIN32
+	//Tx a byte
+	s16Return = s16RM4_I2C_USER__TxByte(u8DeviceAddx, (Luint8)eRegister, u8Byte);
+#else
+	//fake on win32
+	s16Return = 0;
+#endif
+	return s16Return;
+}
+
+Lint16 s16MS5607_I2C__RxU16(Luint8 u8DeviceAddx, E_TSYS01_REGS_T eRegister, Luint16 *pu16)
+{
+	Lint16 s16Return;
+	union
+	{
+		Luint8 u8[2];
+		Luint16 u16;
+	}unT;
+
+	//read two bytes
+#ifndef WIN32
+	s16Return = s16RM4_I2C_USER__RxByteArray(u8DeviceAddx, (Luint8)eRegister, &unT.u8[0], 2U);
+
+	//todo: check mapping
+	*pu16 = unT.u16;
+#else
+	//fake on win32
+	*pu16 = 0U;
+	s16Return = 0;
+#endif
+
+	return s16Return;
+}
+
