3395fde2563de62943d897b98bf55f86e407dcfa
Lachlan Grogan <lachlan@sil3.com.au>
2016-11-11 17:37:52 +1100

    [update] Pi Comms Rx integration

--- FIRMWARE/PROJECT_CODE/LCCM656__RLOOP__PI_COMMS/pi_comms__localdef.h
+++ FIRMWARE/PROJECT_CODE/LCCM656__RLOOP__PI_COMMS/pi_comms__localdef.h
@@ -17,7 +17,9 @@ RLOOP - PI COMMUNICATIONS MODULE
 		//arch
 		#define C_LOCALDEF__LCCM656__USE_ON_RM4								(1U)
 		#define C_LOCALDEF__LCCM656__USE_ON_WIN32							(0U)
-	
+
+		/** enable the receiver side? */
+		#define C_LOCALDEF__LCCM656__ENABLE_RX								(0U)
 
 		/** Testing Options */
 		#define C_LOCALDEF__LCCM656__ENABLE_TEST_SPEC						(0U)
