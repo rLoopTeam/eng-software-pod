92f45949d57a5d0927f62e77074c937efaefe316
Lachlan Grogan <lachlan@sil3.com.au>
2016-11-28 20:54:16 +1100

    [update] added networking stuff

--- FIRMWARE/PROJECT_CODE/LCCM653__RLOOP__POWER_CORE/power_core.h
+++ FIRMWARE/PROJECT_CODE/LCCM653__RLOOP__POWER_CORE/power_core.h
@@ -149,7 +149,8 @@
 		void vPWRNODE_NET__Init(void);
 		void vPWRNODE_NET__Process(void);
 		Luint8 u8PWRNODE_NET__Is_LinkUp(void);
-		void vPWRNODE_NET_RX__RxUDP(Luint8 * pu8Buffer, Luint16 u16Length, Luint16 u16DestPort);
+		void vPWRNODE_NET_RX__RxUDP(Luint8 *pu8Buffer, Luint16 u16Length, Luint16 u16DestPort);
+		void vPWRNODE_NET_RX__RxSafeUDP(Luint8 *pu8Payload, Luint16 u16PayloadLength, Luint16 ePacketType, Luint16 u16DestPort, Luint16 u16Fault);
 
 		//main application state machine
 		void vPWRNODE_SM__Init(void);
