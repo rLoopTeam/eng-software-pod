7f561a86603faf12bbfc8cc5ee275b00afb4f126
Edward 40Chans <edward6chan@gmail.com>
2016-12-11 13:54:14 -0800

    Add locals: get status, get op status, bulk read. Add conversion before reading voltages

--- FIRMWARE/COMMON_CODE/MULTICORE/LCCM650__MULTICORE__ATA6870/ata6870.c
+++ FIRMWARE/COMMON_CODE/MULTICORE/LCCM650__MULTICORE__ATA6870/ata6870.c
@@ -28,6 +28,12 @@
 //main structure
 struct _str6870 sATA6870;
 
+//locals
+static Luint8 uATA6870__GetStatus(Luint8 u8DeviceIndex);
+static Luint8 uATA6870__GetOpStatus(Luint8 u8DeviceIndex);
+static Luint8 uATA6870__BulkRead(Luint8 u8VoltageMode, Luint8 u8TempBit);
+
+
 /***************************************************************************//**
  * @brief
  * Init any ATA6870 systems
@@ -81,6 +87,7 @@ void vATA6870__Init(void)
 
 		//setup the interupts
 		//Only do the important onces for now
+		//TODO do we need to mask undervoltage?
 		u8Temp = 0x16U;
 		vATA6870_LOWLEVEL__Reg_WriteU8(u8Counter, ATA6870_REG__IRQ_MASK, &u8Temp, 1U);
 
@@ -91,8 +98,8 @@ void vATA6870__Init(void)
 
 	}
 
-	u8Temp = 0x01U;
-	vATA6870_LOWLEVEL__Reg_WriteU8(0U, ATA6870_REG__OPERATION, &u8Temp, 1U);
+	//u8Temp = 0x01U;
+	//vATA6870_LOWLEVEL__Reg_WriteU8(0U, ATA6870_REG__OPERATION, &u8Temp, 1U);
 
 
 	//make sure we have our devices
@@ -111,18 +118,101 @@ void vATA6870__Init(void)
  */
 void vATA6870__Process(void)
 {
+	uATA6870__BulkRead(1U, 1U);
+
+	//process any balancer tasks.
+	vATA6870_BALANCE__Process();
+}
+
+// bulk read voltages
+Luint8 uATA6870__BulkRead(Luint8 u8VoltageMode, Luint8 u8TempBit)
+{
+	// volt mode = calibration or regular aquisition. 0 for calibration, 1 for regular.
+	// tempBit = temperature sensor to select. 1 for internal, 0 for external.
+
+
 	Luint8 u8Counter;
 	Luint8 u8VolCounter = 0U;
+	Luint8 u8TempData;
+	Luint8 u8BulkReadError = 0U;
 
-	//read voltages from the battery pack, all 18 modules - each module is being treated as a cell
+	// start conversion by clearing out any exixting comands and issuing our own
 	for(u8Counter = 0U; u8Counter < C_LOCALDEF__LCCM650__NUM_DEVICES; u8Counter++)
 	{
+		//Clear any existing interrupts
+		uATA6870__GetStatus(u8Counter);
+
+		// check and clear exixting commands
+		if (uATA6870__GetOpStatus(u8Counter) != 0U)
+		{
+			//OpClear
+			u8TempData = 0x00u;
+
+			//clear existing command
+			vATA6870_LOWLEVEL__Reg_WriteU8(u8Counter, ATA6870_REG__OPERATION, &u8TempData, 1U);
+
+			vRM4_DELAYS__Delay_mS(5U);
+
+			//Get Status
+			uATA6870__GetStatus(u8Counter);
+
+			//Get Op Status
+			uATA6870__GetOpStatus(u8Counter);
+		}
+
+		//Op Req Data
+		u8TempData = (0x01 | (u8VoltageMode << 1) | (u8TempBit << 3));
+		//Start Conversion
+		vATA6870_LOWLEVEL__Reg_WriteU8(u8Counter, ATA6870_REG__OPERATION, &u8TempData, 1U);
+
+		//vATA6870_CELL__Get_Voltages(u8Counter, &sATA6870.f32Voltage[u8VolCounter], &sATA6870.f32NTCTemperatureReading[u8Counter]);
+		//u8VolCounter += C_ATA6870__MAX_CELLS;
+	}
+
+	// scan the bus for a dataready interrupt and read data into adc value arrays
+	for(u8Counter = 0U; u8Counter < C_LOCALDEF__LCCM650__NUM_DEVICES; u8Counter++)
+	{
+		//TODO
+
+		// 8.2ms conversion time according to datasheet.
+		vRM4_DELAYS__Delay_mS(10U);
+	}
+
+	// read data
+	for(u8Counter = 0U; u8Counter < C_LOCALDEF__LCCM650__NUM_DEVICES; u8Counter++)
+	{
+
+
 		vATA6870_CELL__Get_Voltages(u8Counter, &sATA6870.f32Voltage[u8VolCounter], &sATA6870.f32NTCTemperatureReading[u8Counter]);
 		u8VolCounter += C_ATA6870__MAX_CELLS;
 	}
 
-	//process any balancer tasks.
-	vATA6870_BALANCE__Process();
+	return u8BulkReadError;
+}
+
+// read the status register
+Luint8 uATA6870__GetStatus(Luint8 u8DeviceIndex)
+{
+	Luint8 u8Return = 0xFFU;
+	vATA6870_LOWLEVEL__Reg_WriteU8(u8DeviceIndex, ATA6870_REG__IRQ_STATUS, &u8Return, 1U);
+
+	return u8Return;
+}
+
+// get operation status
+Luint8 uATA6870__GetOpStatus(Luint8 u8DeviceIndex)
+{
+	//useage
+	// output:
+	// 0 = no operation
+	// 1 = ongoing operation
+	// 2 = operation finished
+	// 3 = operation failed/was cancled. result not available.
+
+	Luint8 u8Return = 0xFFU;
+	vATA6870_LOWLEVEL__Reg_ReadU8(u8DeviceIndex, ATA6870_REG__OP_STATUS, &u8Return, 1U);
+
+	return u8Return;
 }
 
 
